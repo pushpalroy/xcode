8fa391bdc0514108d61a8c2a874b8701567be930
Pushpal Roy <pushpalroy2007@gmail.com>
2020-03-01 16:12:13 +0530

    Add lazy vs collection comparison

---
+++ app/src/main/java/com/example/xcode/lambdas/sequences-comparison.kt
@@ -0,0 +1,31 @@
+package com.example.xcode.lambdas
+
+import kotlin.system.measureTimeMillis
+
+fun main() {
+
+  var sum = 0L
+  var lazySum = 0L
+
+  val veryLongList = (1..999999L).toList()
+
+  val collectionTime = measureTimeMillis {
+    sum = veryLongList
+        .filter { it > 10 }
+        .map { it * 3 }
+        .take(1000)
+        .sum()
+  }
+
+  val lazyTime = measureTimeMillis {
+    lazySum = veryLongList
+        .asSequence()
+        .filter { it > 10 }
+        .map { it * 3 }
+        .take(1000)
+        .sum()
+  }
+
+  println("Collection: Time: $collectionTime ms, Sum: $sum")
+  println("Lazy: Time: $lazyTime ms, Sum: $lazySum")
+}
